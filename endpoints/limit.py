cf51e8c0073dad7cc191b1a9d7ef69073e743d70
manishweb23 <manish@web23.io>
2024-02-02 03:03:41 +0530

    file req

--- endpoints/limit.py
+++ endpoints/limit.py
@@ -28,7 +28,7 @@ async def change_limit(request:Request,payload:limit,limit_id:int):
     response = await update_limit(limit_id,**payload)
     return response
 
-@router.get("/limit/user_id/{user_id}")
+@router.get("/limit/user/{user_id}")
 async def get_limit(request:Request,user_id:int):
     response = await fetch_limit(user_id)
     return response 

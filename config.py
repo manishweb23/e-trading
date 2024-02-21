79d5b31e6a70900ee1e63c1d6b091a14fc503ca3
manishweb23 <manish@web23.io>
2024-02-21 07:24:44 +0530

    config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ redirect_uri = 'https://web23.com'
 access_token = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI2MkFIQVAiLCJqdGkiOiI2NWQ1NTAxNTdkMmNlMTI4NGI1OTNjMDAiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE3MDg0Nzg0ODUsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTcwODU1MjgwMH0.yyhRadgA-p8TTLTtZaz6sjduISlln8nKC0Z41bTU-kI"
 
 # db credentials
-DATABASE_URL = "postgresql://postgres:123456@localhost/db_dubba"
+# DATABASE_URL = "postgresql://postgres:123456@localhost/db_dubba"
 SECRET_KEY = "8586493dc97a4f44a7677c23a81fd150"
 ALGORITHM = "HS256"
 
-# DATABASE_URL = "postgresql://postgres:123123@127.0.0.1:5432/db_dubba"
+DATABASE_URL = "postgresql://postgres:123123@127.0.0.1:5432/db_dubba"

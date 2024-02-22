442d4abb9bd3045abdd49d9d3d04ba635a77ec5b
manishweb23 <manish@web23.io>
2024-02-22 12:35:06 +0530

    hytf

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ redirect_uri = 'https://web23.com'
 access_token = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI2MkFIQVAiLCJqdGkiOiI2NWQ2ZjEwZTQ2YjYwOTJhNGM1ZGEwZGYiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE3MDg1ODUyMzAsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTcwODYzOTIwMH0.lh0GXckzYxFfj9_VrxexzGmkDyskBGHGa3f3VgJhnyM"
 
 # db credentials
-DATABASE_URL = "postgresql://postgres:123456@localhost/db_dubba"
+# DATABASE_URL = "postgresql://postgres:123456@localhost/db_dubba"
 SECRET_KEY = "8586493dc97a4f44a7677c23a81fd150"
 ALGORITHM = "HS256"
 
-# DATABASE_URL = "postgresql://doadmin:AVNS_-YtwVSF3KJKWBpVMt-k@db-postgresql-blr1-39916-do-user-6620209-0.c.db.ondigitalocean.com:25060/defaultdb"
+DATABASE_URL = "postgresql://doadmin:AVNS_-YtwVSF3KJKWBpVMt-k@db-postgresql-blr1-39916-do-user-6620209-0.c.db.ondigitalocean.com:25060/defaultdb"
